10cbf37265690b3288d0ad8a9b81c655a87fe1c0
JayVae <919433524@qq.com>
2021-03-22 19:01:36 +0800

    ftp information

---
+++ src/test/java/com/mmall/util/MD5UtilTest.java
@@ -0,0 +1,95 @@
+package com.mmall.util; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* MD5Util Tester. 
+* 
+* @author JS 
+* @since 03/22/2021 
+* @version 1.0 
+*/ 
+public class MD5UtilTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: MD5EncodeUtf8(String origin) 
+* 
+*/ 
+@Test
+public void testMD5EncodeUtf8() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: byteArrayToHexString(byte b[]) 
+* 
+*/ 
+@Test
+public void testByteArrayToHexString() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = MD5Util.getClass().getMethod("byteArrayToHexString", byte.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: byteToHexString(byte b) 
+* 
+*/ 
+@Test
+public void testByteToHexString() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = MD5Util.getClass().getMethod("byteToHexString", byte.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: MD5Encode(String origin, String charsetname) 
+* 
+*/ 
+@Test
+public void testMD5Encode() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = MD5Util.getClass().getMethod("MD5Encode", String.class, String.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
